a1c7190a1d31a71371e34efef673879f8bfb84ca
Chris 'Caboose' Marasco <cxm6467@gmail.com>
2021-04-16 04:27:30 -0400

    Set Up MongoDB Added A Book Model Added A Book Router

--- node/app.js
+++ node/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
-
+const db = mongoose.connect('mongodb://localhost/bookApi')
+const bookRouter = express.Router();
 const port = process.env.PORT || 4000;
+const Book = require('./models/BookModel');
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      } else {
+        return res.json(books);
+      }
+    });
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon Api!');
